db86e6d3c83e2e3a597d0fd5919dd769a7319388
Piyush Pamnani <piyushpamnani46@gmail.com>
2023-09-10 21:02:18 +0530

    Product Section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,11 +3,13 @@ const Button = ({ label, iconURL }) => {
     <button className="flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red border-coral-red text-white rounded-full">
       {label}
 
-      <img
-        src={iconURL}
-        alt="right arrow"
-        className="ml-2 rounded-full w-5 h-5"
-      />
+      {iconURL && (
+        <img
+          src={iconURL}
+          alt="right arrow"
+          className="ml-2 rounded-full w-5 h-5"
+        />
+      )}
     </button>
   );
 };
